55b284ce9a9299ef4dd292d70b83bf3a3f26b5ae
Voraprat Rattana <voraprat.r@dosetech.co>
2021-05-24 14:06:08 +0700

    [Add] cookie-consent lib by VorapratR

--- public/yett.js
+++
@@ -1,8 +0,0 @@
-window.YETT_BLACKLIST = [
-        /www\.google-analytics\.com/,
-        /www\.googletagmanager\.com/
-    ]
-    // Or a whitelist
-window.YETT_WHITELIST = [
-    /my-whitelisted-domain/
-]
